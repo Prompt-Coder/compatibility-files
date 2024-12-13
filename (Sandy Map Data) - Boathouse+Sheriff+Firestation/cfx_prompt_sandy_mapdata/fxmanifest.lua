7f8b29f1af8d3e9f07e97e596ae4ef1b623e64a1
Prompt X <stanislav.semenov.2016@mail.ru>
2024-12-12 17:52:14 -0800

    Add folder as branch: (Sandy Map Data) - Boathouse+Sheriff+Firestation

---
+++ (Sandy Map Data) - Boathouse+Sheriff+Firestation/cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description '(Sandy_Map_Data)_-_Boathouse+Sheriff+Firestation'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+server_scripts{
+    'sv_MapDataHandler.lua'
+}
---
+++ (Sandy Map Data) - Boathouse+Sheriff+Firestation/cfx_prompt_sandy_mapdata/sv_MapDataHandler.lua
@@ -0,0 +1,37 @@
+local maps = {
+    "cfx_chuz_sandy_shores_boat_house",
+    "prompt_sandy_sheriff",
+    "cfx_prompt_sandy_shores_fire_department"
+}
+
+local events = {}
+
+CreateThread(function()
+    local exists = false 
+    TriggerEvent("lyn-mapdata:exists", function(exists)
+        if exists then
+            exists = true
+        end
+    end)
+
+    if exists == true then 
+        print("^6[Prompt]^1 Map data already exists. There must be only one mapdata installed!^0")
+    end
+end)
+
+RegisterNetEvent("lyn-mapdata:exists", function(cb)
+    cb(true)
+end)
+
+for i = 1, #maps do
+    local eventName = maps[i] .. ":mapDataExists"
+    if Debug == true then 
+        print("Creating event: ", eventName)
+    end
+    local event = RegisterNetEvent(eventName, function(cb) 
+        cb(true)
+    end)
+    table.insert(events, event)
+end
+
+-- 
